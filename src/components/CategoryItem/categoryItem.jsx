d70a1d1e5f03235482a4f77bd64fd16271edd67a
Vyshniakov Ihor <ihor.vyshniakov@qlicks.nl>
2024-05-19 22:37:53 +0300

    added my styling and removed useless buttons

--- src/components/CategoryItem/categoryItem.jsx
+++ src/components/CategoryItem/categoryItem.jsx
@@ -3,24 +3,35 @@ import './categoryItem.scss'
 import { go } from '../../App'
 
 const CategoryItem = ({
-	idCategory,
 	strCategory,
 	strCategoryThumb,
 	strCategoryDescription
 }) => {
 	return (
-		<div className='card'>
-			<div className='card-image'>
-				<img src={strCategoryThumb} alt={strCategory} />
-			</div>
+		<div className='card lime lighten-4'>
+			<Link
+				to={go(`/category/${strCategory}`)}
+				style={{ color: 'black' }}
+			>
+				<div className='card-image waves-effect waves-block waves-light'>
+					<img src={strCategoryThumb} alt={strCategory} />
+				</div>
+			</Link>
 			<div className='card-content'>
-				<span className='card-title'>{strCategory}</span>
-				<p>{strCategoryDescription.slice(0, 60)}...</p>
+				<span
+					className='card-title activator grey-text text-darken-4'
+					style={{ margin: '0' }}
+				>
+					{strCategory}
+					<i className='material-icons right'>more_vert</i>
+				</span>
 			</div>
-			<div className='card-action'>
-				<Link to={go(`/category/${strCategory}`)} className='btn'>
-					Watch category
-				</Link>
+			<div className='card-reveal'>
+				<span className='card-title grey-text text-darken-4'>
+					{strCategory}
+					<i className='material-icons right'>close</i>
+				</span>
+				<p>{strCategoryDescription.slice(0, 180)}...</p>
 			</div>
 		</div>
 	)
--- src/components/MealItem/mealItem.jsx
+++ src/components/MealItem/mealItem.jsx
@@ -3,17 +3,14 @@ import { go } from '../../App'
 
 const MealItem = ({ idMeal, strMeal, strMealThumb }) => {
 	return (
-		<div className='card'>
-			<div className='card-image'>
-				<img src={strMealThumb} alt={strMeal} />
-			</div>
+		<div className='card lime lighten-4'>
+			<Link to={go(`/meal/${idMeal}`)}>
+				<div className='card-image'>
+					<img src={strMealThumb} alt={strMeal} />
+				</div>
+			</Link>
 			<div className='card-content'>
 				<span className='card-title'>{strMeal}</span>
-			</div>
-			<div className='card-action'>
-				<Link to={go(`/meal/${idMeal}`)} className='btn'>
-					Watch recipe
-				</Link>
 			</div>
 		</div>
 	)
